29f63cc8316685042a55af0c078fb6f51b812d4e
xJONATHANw_Dev <96226740+HaNKMF@users.noreply.github.com>
2025-02-18 17:25:49 +0700

    Synex-KUB.lua

--- Synex-KUB.lua
+++ Synex-KUB.lua
@@ -4,7 +4,7 @@ local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.
 
 local Window = Fluent:CreateWindow({
     Title = "Rock Fruit | SynexHUB ",
-    SubTitle = "by Deoxy",
+    SubTitle = "by xJONATHANw",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
